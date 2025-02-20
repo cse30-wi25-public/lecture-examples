e182ab9a20e8b9a82a124a039a71f1280b14242e
bwchin <bwchin@gmail.com>
2025-02-20 09:31:44 -0800

    fixed main.c

--- machinecode/main.c
+++ machinecode/main.c
@@ -5,6 +5,6 @@ int main(int argc, char **argv){
    long a = strtol(argv[1], NULL, 10);
    long b = strtol(argv[2], NULL, 10);
    int c = sum(a, b);
-   printf(“%d + %d = %d\n”, a, b, c);
+   printf("%d + %d = %d\n", a, b, c);
 }
 
