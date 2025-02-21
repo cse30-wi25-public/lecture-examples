d16702265b59352185cc33a54a92ac03311ea217
bwchin <bwchin@gmail.com>
2025-02-21 09:50:24 -0800

    use argc

--- machinecode/main.c
+++ machinecode/main.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 extern int sum(int, int);
 int main(int argc, char **argv){
-   int a = strtol(argv[1], NULL, 10);
-   int b = strtol(argv[2], NULL, 10);
+   int a = strtol(argv[argc-2], NULL, 10);
+   int b = strtol(argv[argc-1], NULL, 10);
    int c = sum(a, b);
    printf("%d + %d = %d\n", a, b, c);
 }
