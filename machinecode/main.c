8d47f2c0760cdeae52ed13a3703977702907bae4
bwchin <bwchin@gmail.com>
2025-02-21 09:47:16 -0800

    fixed warning

--- machinecode/main.c
+++ machinecode/main.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 extern int sum(int, int);
 int main(int argc, char **argv){
-   long a = strtol(argv[1], NULL, 10);
-   long b = strtol(argv[2], NULL, 10);
+   int a = strtoi(argv[1], NULL, 10);
+   int b = strtoi(argv[2], NULL, 10);
    int c = sum(a, b);
    printf("%d + %d = %d\n", a, b, c);
 }
